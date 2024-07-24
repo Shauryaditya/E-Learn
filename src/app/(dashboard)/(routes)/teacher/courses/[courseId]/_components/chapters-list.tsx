4786c2e6e8c7ea2a68ea00991e6711305eb2ab2b
Shauryaditya <shauryadityab@gmail.com>
2024-07-24 23:49:03 +0530

    chapters

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
@@ -2,6 +2,14 @@
 
 import { Chapter } from "@prisma/client"
 import { useEffect, useState } from "react";
+import {DragDropContext,
+    Droppable,
+    Draggable,
+    DropResult
+} from "@hello-pangea/dnd"
+
+import { cn } from "@/lib/utils";
+import { Grid, Grip } from "lucide-react";
 
 interface ChaptersListProps {
     items: Chapter[]
@@ -21,12 +29,49 @@ export const ChaptersList = ({
         setIsMounted(true);
     },[])
 
+    useEffect(() => {
+        setChapters(items)
+    },[items])
+
     if(!isMounted){
         return null;
     }
     return (
-        <div className="">
-            Chapters List
-        </div>
+      <DragDropContext onDragEnd={() => {}}>
+        <Droppable droppableId="chapters" >
+            {(provided) => (
+                <div {...provided.droppableProps} ref={provided.innerRef}>
+                    {chapters.map((chapter, index) =>(
+                        <Draggable 
+                        key={chapter.id} 
+                        draggableId={chapter.id} 
+                        index={index}
+                        >
+                            {(provided) => (
+                                <div className={cn(
+                                    "flex items-center gap-x-2 bg-slate-200 border-slate-200 border text-slate-700 rounded-md mb-4 text-sm",
+                                    chapter.isPublished && "bg-sky-100 border-sky-200 text-sky-700"
+                                )}
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                >
+                                    <div className={cn(
+                                        "px-2 py-3 border-r-slate-200 hover:bg-slate-300 rounded-l-md transition",
+                                        chapter.isPublished && "bg-sky-100 border-sky-200 text-sky-200"
+                                    )}
+                                    {...provided.dragHandleProps}>
+                                        <Grip />
+                                    </div>
+
+                                </div>
+                            )}
+
+                        </Draggable>
+
+                    ) )}
+                </div>
+            )}
+        </Droppable>
+      </DragDropContext>
         )
 }
