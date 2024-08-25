7f4f0230fcd0ad381fe2c781fbb00899c48b3ac4
Shauryaditya <shauryadityab@gmail.com>
2024-08-25 22:57:44 +0530

    search query

---
+++ src/app/(dashboard)/(routes)/search/_components/category-item.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import qs from "query-string";
+import { cn } from "@/lib/utils";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+import { IconType } from "react-icons";
+import { title } from "process";
+import { url } from "inspector";
+
+interface CategoryItemProps {
+  label: string;
+  value?: string;
+  icon?: IconType;
+}
+
+export const CategoryItem = ({
+  label,
+  value,
+  icon: Icon,
+}: CategoryItemProps) => {
+  const pathName = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const currentCategoryId = searchParams.get("CategoryId");
+  const currentTitle = searchParams.get("title");
+
+  const isSelected = currentCategoryId === value;
+
+  const onClick = () => {
+    const url = qs.stringifyUrl(
+      {
+        url: pathName,
+        query: {
+          title: currentTitle,
+          CategoryId: isSelected ? null : value,
+        },
+      },
+      { skipNull: true, skipEmptyString: true }
+    );
+    router.push(url);
+  };
+  return (
+    <div className="">
+      <button
+      onClick={onClick}
+        className={cn(
+          "py-2 px-3 text-sm border border-slate-200 rounded-full flex items-center gap-x-1 hover:border-sky-700 transition",
+          isSelected && "border-sky-700 bg-sky-200/20 text-sky-800"
+        )}
+      >
+        {Icon && <Icon size={20} />}
+        <div className="truncate">{label}</div>
+      </button>
+    </div>
+  );
+};
