0fbbeeb39144614976e4515194899bffd5cb5342
Shauryaditya <shauryadityab@gmail.com>
2024-09-01 16:38:21 +0530

    "Added @radix-ui/react-separator dependency and updated package-lock.json, package.json, and several code files with new imports, components, and functionality."

--- src/app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ src/app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -3,6 +3,10 @@ import { Banner } from "@/components/banner";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { VideoPlayer } from "./_components/video-player";
+import { CourseEnrollButton } from "./_components/course-enroll-bitton";
+import { Separator } from "@/components/ui/separator";
+import { Preview } from "@/components/preview";
+import { File } from "lucide-react";
 
 const ChapterIdPage = async ({
   params,
@@ -40,19 +44,57 @@ const ChapterIdPage = async ({
         <Banner variant="success" label="You already completed this chapter" />
       )}
       {isLocked && (
-        <Banner variant="warning" label="You need to purchase this course to watch this chapter" />
+        <Banner
+          variant="warning"
+          label="You need to purchase this course to watch this chapter"
+        />
       )}
       <div className="flex flex-col max-w-4xl mx-auto pb-20">
         <div className="p-4">
           <VideoPlayer
-          chapterId={params.chapterId}
-          title={chapter.title}
-          courseId={params.courseId}
-          nextChapterId={nextChapter?.id!}
-          playbackId={muxData?.playbackId!}
-          isLocked={isLocked}
-          completeOnEnd={completeOnEnd}
+            chapterId={params.chapterId}
+            title={chapter.title}
+            courseId={params.courseId}
+            nextChapterId={nextChapter?.id!}
+            playbackId={muxData?.playbackId!}
+            isLocked={isLocked}
+            completeOnEnd={completeOnEnd}
           />
+        </div>
+        <div className="">
+          <div className="p-4 flex flex-col md:flex-row items-center justify-between">
+            <h2 className="text-2xl font-semibold mb-2">{chapter.title}</h2>
+            {purchase ? (
+              <div className="">//Todo: Add Course Progress Button</div>
+            ) : (
+              <CourseEnrollButton
+                courseId={params.courseId}
+                price={course.price!}
+              />
+            )}
+          </div>
+          <Separator />
+          <div className="">
+            <Preview value={chapter.description!} />
+          </div>
+          {!!attachments.length && (
+            <>
+              <Separator />
+              <div className="p-4">
+                {attachments.map((attachment) => (
+                  <a
+                    href={attachment.url}
+                    target="_blank"
+                    key={attachment.id}
+                    className="flex items-center p-3 w-full bg-sky-200 border text-sky-700 rounded-md hover:underline"
+                  >
+                    <File />
+                    <p className="line-clamp-1">{attachment.name}</p>
+                  </a>
+                ))}
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
