9dce634b32912df8a5c1151628d3c92f483fb9fd
Shauryaditya <shauryadityab@gmail.com>
2024-09-08 23:45:51 +0530

    "Updated package-lock.json and package.json to include razorpay dependency, added RazorpayCustomer and RazorpayOrder models to schema.prisma, and updated axios and follow-redirects versions."

---
+++ src/lib/razorpay.ts
@@ -0,0 +1,8 @@
+// pages/api/create-order.js
+import Razorpay from 'razorpay';
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID!,
+  key_secret: process.env.RAZORPAY_KEY_SECRET!,
+});
+
