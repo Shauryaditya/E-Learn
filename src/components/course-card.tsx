9f610e85625bb8cdea455da1761f72a2fcfeb9e1
Shauryaditya <shauryadityab@gmail.com>
2025-01-22 21:02:28 +0530

    Refactor CourseCard component layout and improve progress display logic

--- src/components/course-card.tsx
+++ src/components/course-card.tsx
@@ -24,7 +24,7 @@ export const CourseCard = ({
   progress,
   category,
 }: CourseCardProps) => {
-  console.log("Progress>>??",progress)
+  console.log("Progress>>??", progress);
   return (
     <Link href={`/courses/${id}`}>
       <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
@@ -32,30 +32,29 @@ export const CourseCard = ({
           <Image fill className="object-cover" alt={title} src={imageUrl} />
         </div>
         <div className="flex flex-col pt-2">
-            <div className="text-lg md:text-base font-medium group-hover:text-sky-700 transition line-clamp-2">
-                {title}
+          <div className="text-lg md:text-base font-medium group-hover:text-sky-700 transition line-clamp-2">
+            {title}
+          </div>
+          <p className="text-xs text-muted-foreground">{category}</p>
+          <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
+            <div className="flex items-center gap-x-1 text-slate-500">
+              <IconBadge size="sm" icon={BookOpen} />
+              <span>
+                {chaptersLength} {chaptersLength === 1 ? "Chapter" : "Chapters"}
+              </span>
             </div>
-            <p className="text-xs text-muted-foreground">{category}</p>
-            <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
-                <div className="flex items-center gap-x-1 text-slate-500">
-                    <IconBadge size="sm" icon={BookOpen} />
-                    <span>
-                        {chaptersLength} {chaptersLength === 1 ? "Chapter" : "Chapters"}
-                    </span>
-                </div>
-
-            </div>
-            {progress !== null ? (
-                <CourseProgress 
-                variant={progress === 100 ? "success" : "default"}
-                size="sm"
-                value={progress}
-                />
-            ): (
-                <p className="text-md md:text-sm font-medium">
-                    {formatPrice(price)}
-                </p>
-            )}
+          </div>
+          {progress !== null ? (
+            <CourseProgress
+              variant={progress === 100 ? "success" : "default"}
+              size="sm"
+              value={progress}
+            />
+          ) : (
+            <p className="text-md md:text-sm font-medium">
+              <span className="line-through">{formatPrice(price)}</span> Free
+            </p>
+          )}
         </div>
       </div>
     </Link>
