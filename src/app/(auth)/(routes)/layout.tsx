fe33fa6c638940c31fa6ee77d6aff325ca519669
Shauryaditya <shauryadityab@gmail.com>
2026-01-19 12:53:08 +0530

    fix: Adjust margin in AuthLayout for improved spacing

--- src/app/(auth)/(routes)/layout.tsx
+++ src/app/(auth)/(routes)/layout.tsx
@@ -4,7 +4,7 @@ const AuthLayout = ({
     children: React.ReactNode
 }) => {
   return (
-    <div className="h-full flex items-center justify-center mt-8">
+    <div className="h-full flex items-center justify-center mt-9">
         {children}
     </div>
   )
